2ab77e86000e4a1926a94ccbf6901eb8aa2cae81
wanniga <41385413+wanniga@users.noreply.github.com>
2020-12-03 21:16:25 +0700

    Create myBot.js

---
+++ myBot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js'); //เรียก discord.js มาใช้
+const botRem = new Discord.Client(); //ประกาศ client ขึ้นมา
+//event นี้ทำงานเมื่อ login สำเร็จ
+botRem.on('ready', () => {
+  console.log('Rem ready!');
+});
+//รอรับ event message เวลามีข้อความโผล่มาในแชท function นี้ก็จะทำงาน
+botRem.on('message', message => { 
+  if (message.content === 'ฉันรักเอมิเลีย') {
+    message.reply('ถึงยังไงเรมก็รักสุบารุค่ะ');
+  }
+});
+botRem.login('เอา token มาใส่ตรงนี้');
